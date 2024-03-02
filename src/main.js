1840f1c88861ae89571d4ebe181918456683749c
cherites0610 <cherites0610@gmail.com>
2024-03-02 12:34:02 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import 'vant/lib/index.css';
 import App from './App.vue'
 import { router } from './router';
 import { createPinia } from 'pinia'
+import moment from 'moment';
 
 const pinia = createPinia()
 const app = createApp(App);
-
+moment.updateLocale('en', {
+    weekdaysShort: ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+});
 app.use(pinia)
 app.use(router)
 app.mount('#app');
--- src/views/availabilityScheduling.vue
+++ src/views/availabilityScheduling.vue
@@ -8,7 +8,7 @@
         </van-picker-group>
     </van-popup>
     <van-cell-group v-if="cellShow">
-        <van-cell center :key="index" :title="key.format('YYYY-MM-DD(星期E)')" v-for="(key,index) in cycleDate">
+        <van-cell center :key="index" :title="key.format('YYYY-MM-DD(ddd)')" v-for="(key, index) in cycleDate">
             <template #value>
                 <van-space>
                     <van-field v-model="inputData[Number(index)]" placeholder="请输入給班時間" />
@@ -28,7 +28,7 @@ const showPopup = () => {
     popupShow.value = true
 }
 const inputData = ref([]);
-const selectedDate = ref([new Date().getFullYear(), new Date().getMonth()+1, new Date().getDate()]);
+const selectedDate = ref([new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()]);
 const cycle = ref(['7']);
 const cycleDate = ref([]);
 const cycleText = [
@@ -39,46 +39,46 @@ const cycleText = [
 ]
 
 const formatter = (type, option) => {
-      if (type === 'year') {
+    if (type === 'year') {
         option.text += '年';
-      }
-      if (type === 'month') {
+    }
+    if (type === 'month') {
         option.text += '月';
-      }
-      if( type ==='day'){
+    }
+    if (type === 'day') {
         option.text += '日';
-      }
-      return option;
-    };
+    }
+    return option;
+};
 
 const onConfirm = () => {
-    popupShow.value=false
+    popupShow.value = false
 
-    cycleDate.value=[];
+    cycleDate.value = [];
 
-    for(let i=0;i<cycle.value;i++){
+    for (let i = 0; i < cycle.value; i++) {
         let tempDate = new moment(selectedDate.value);
         tempDate.subtract(1, 'months');
-        tempDate.add(i,'days')
+        tempDate.add(i, 'days')
         cycleDate.value.push(tempDate);
     }
 
-    cellShow.value=true;
+    cellShow.value = true;
 };
 
 const exportData = () => {
     let outputText = '';
-    
-    for(let i=0;i<cycleDate.value.length;i++){
-        outputText+=cycleDate.value[i].format('MM/DD')+' : ';
-        if(inputData.value[i]){
-            outputText+=inputData.value[i]
-        }else{
-            outputText+='X'
+
+    for (let i = 0; i < cycleDate.value.length; i++) {
+        outputText += cycleDate.value[i].format('MM/DD') + ' : ';
+        if (inputData.value[i]) {
+            outputText += inputData.value[i]
+        } else {
+            outputText += 'X'
         }
-        outputText+='\n'
+        outputText += '\n'
     }
-    showDialog({ title:"給班表",message: outputText });
+    showDialog({ title: "給班表", message: outputText });
 }
 
 const onCancel = () => {
